743d5bbab42aa79e8c081023b7d308af03147270
mohit23x <mohit.lodha@razorpay.com>
2022-10-10 11:44:24 +0530

    update git ignore

--- packages/react-native-apollo-devtools-client/lib/typescript/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ApolloClientType, Callback } from './typings';
-export declare const apolloDevToolsInit: (client: ApolloClientType, { onConnect, }: {
-    onConnect?: Callback | undefined;
-}) => void;
--- packages/react-native-apollo-devtools-client/lib/typescript/initializeFlipperUtils.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Flipper } from 'react-native-flipper';
-import type { ApolloClientType } from './typings';
-export declare const initializeFlipperUtils: (flipperConnection: Flipper.FlipperConnection, apolloClient: ApolloClientType) => void;
